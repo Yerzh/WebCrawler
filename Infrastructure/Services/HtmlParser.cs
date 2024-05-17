640609cafe9bc1d4fd5470a16b2f282f9e3172e1
Yerzhan Salkynbayev <yerzhan.salkynbayev@dataart.com>
2024-05-17 17:04:28 +0500

    Implemented HtmlParser class

---
+++ Infrastructure/Services/HtmlParser.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Services;
+using HtmlAgilityPack;
+
+namespace Infrastructure.Services;
+
+public class HtmlParser : IHtmlParser
+{
+    public async Task ParseAsync(string pageUrl)
+    {
+        var web = new HtmlWeb();
+        var htmlDoc = await web.LoadFromWebAsync(pageUrl);
+        var urls = ExtractUrlsFromPage(htmlDoc);
+
+    }
+
+    private static IList<string> ExtractUrlsFromPage(HtmlDocument doc)
+    {
+        var result = new List<string>();
+
+        var hrefNodes = doc.DocumentNode.SelectNodes("//a[@href]");
+        if (hrefNodes is null)
+        {
+            return result;
+        }
+
+        foreach (var node in hrefNodes)
+        {
+            string hrefValue = node.GetAttributeValue("href", string.Empty);
+            result.Add(hrefValue);
+        }
+
+        return result;
+    }
+}
