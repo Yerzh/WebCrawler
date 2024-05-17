5b6a8ae53879cbb493cc50da6fc482017a34b258
Yerzhan Salkynbayev <yerzhan.salkynbayev@dataart.com>
2024-05-17 18:18:45 +0500

    Added gitignore file

--- HtmlDownloader/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ HtmlDownloader/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640609cafe9bc1d4fd5470a16b2f282f9e3172e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1d589bc3f7766a53b7bdfbe03bd624b349aac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
